e4774c44f7a8d6e3eb9314240b5088dd71d53ff1
Luiz Gall <luiz_gall@hotmail.com>
2018-01-19 14:25:59 -0200

    mudado product para users

--- internet-banking/src/server/routes/api.js
+++ internet-banking/src/server/routes/api.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 // Models
-var Product = require('../models/product');
+var Product = require('../models/users');
 
 //Product.remove({}, function(err) { 
 //    console.log('collection removed') 
@@ -11,26 +11,24 @@ var Product = require('../models/product');
 
 
 
-//carregar seeds 
+//carregar seeds se banco estiver vazio
 Product.find({}, function (err, docs) {
    if(docs.length === 0){ // Se a coleção estiver vazia, popula o banco com os dados do seed.json
       var fs = require('../seed.json');
         for(let user of fs){
-            var instancia = new Product(user);
+            var instancia = new Product(user)
             instancia.save(function (err) {
             if (err) return handleError(err)
             });}
-   } else {
    }
-  });
+  })
 
 
-//check
 
 
 // Routes
 Product.methods(['get', 'put', 'post', 'delete']);
-Product.register(router, '/productss');
+Product.register(router, '/users');
 
 // Return router
 module.exports = router;
