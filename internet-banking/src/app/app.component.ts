d1b5c778279be3e3f34fa9b1956f95eadb7d3829
Dante Webmaster <dantewebmaster@outlook.com>
2018-01-22 03:12:44 -0200

    Rotas de navegação do app criadas

--- internet-banking/src/app/app.component.ts
+++ internet-banking/src/app/app.component.ts
@@ -1,35 +1,38 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import{ServiceService} from './services/service.service'
-import{LoginService} from './services/login.service'
+import{ ServiceService } from './services/service.service'
+import{ LoginService } from './services/login.service'
 import { TransferenciaService } from './services/transferencia.service';
- 
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
-  constructor(private serviceService:ServiceService, private loginService:LoginService, private transferenciaService:TransferenciaService){
+	
+	constructor(
+		private serviceService: ServiceService, 
+		private loginService: LoginService, 
+		private transferenciaService: TransferenciaService
+	) {}
 
-  }
-  title = 'app';
-
-
-
-  ngOnInit(){
-
-    this.serviceService.doGet();
-    let account = 1001
-    let password = 123456
-    let value = 1
-    let dest = 1002
-
-    this.loginService.tryLogin(account, password);
-    this.transferenciaService.transfer(account, password, value, dest);
-    this.serviceService.doGet();
-  }
-
+	title = 'app';
+	
+	
+	
+	ngOnInit(){
+		
+		this.serviceService.doGet();
+		let account = 1001
+		let password = 123456
+		let value = 1
+		let dest = 1002
+		
+		this.loginService.tryLogin(account, password);
+		this.transferenciaService.transfer(account, password, value, dest);
+		this.serviceService.doGet();
+	}
+	
 }
---
+++ internet-banking/src/app/guards/login.guard.ts
@@ -0,0 +1,23 @@
+// import { Injectable } from '@angular/core';
+// import { CanActivate, Router } from '@angular/router';
+// import { LoginService } from '../services/login.service';
+
+// @Injectable()
+// export class LoginGuard implements CanActivate {
+
+// 	isLoggedIn: boolean;
+
+// 	constructor(
+// 		private router: Router,
+// 		private loginService: LoginService
+// 	) { }
+
+// 	// canActivate(): boolean {
+// 	// 	if (this.loginService.tryLogin(1002, 123450)) {
+// 	// 		return true;
+// 	// 	} else {
+// 	// 		this.router.navigate(['/login']);	
+// 	// 		return false;
+// 	// 	}
+// 	// }
+// }
