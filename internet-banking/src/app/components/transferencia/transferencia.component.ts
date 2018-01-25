2db5613ef1d6a78784705d42ff42fb2fb2cdd68d
Dante Webmaster <dantewebmaster@outlook.com>
2018-01-24 23:22:08 -0200

    Código organizado e editorconfig atualizado

--- internet-banking/src/app/components/transferencia/transferencia.component.ts
+++ internet-banking/src/app/components/transferencia/transferencia.component.ts
@@ -10,38 +10,43 @@ import { MatTableDataSource } from '@angular/material';
 	styleUrls: ['./transferencia.component.scss']
 })
 export class TransferenciaComponent implements OnInit {
-	
-	displayedColumns = ['type', 'date', 'value'];
-	dataSource = new MatTableDataSource<Statement>(ESTATEMENT_DATA);
+
+	displayedColumns = [];
+	dataSource;
 	
 	data = {
-		username:"",
-		balance:0,
+		username: "",
+		balance: 0,
 		account: 0,
 		logs: []
 	}
 	
 	toAccount
 	value
-	constructor(private http:HttpClient,
-	private transferenciaService: TransferenciaService,
-	private router:Router) { }
+
+	constructor(
+		private http: HttpClient,
+		private transferenciaService: TransferenciaService,
+		private router: Router
+	) {}
 	
 	ngOnInit() {
 		let url = `http://localhost:3000/api/user`;
 		this.http.post(url, {token: localStorage.getItem("auth-token")})
-		.subscribe(
-			res => {
-				this.data.username = res['username']
-				this.data.balance = parseInt(res['balance'])
-				this.data.account = res["account"]
-				this.data.logs =  res['logs'] 
-			}
-		)
+			.subscribe(
+				res => {
+					this.data.username = res['username']
+					this.data.balance = parseInt(res['balance'])
+					this.data.account = res["account"]
+					this.data.logs =  res['logs'] 
+				}
+			)
 	}
 	
 	onSubmit(){
-		this.transferenciaService.transfer(localStorage.getItem("auth-token"), parseInt(this.value), this.toAccount, this.afterSubmit);
+		this.transferenciaService.transfer(
+			localStorage.getItem("auth-token"), parseInt(this.value), this.toAccount, this.afterSubmit
+		);
 	}
 	
 	afterSubmit = (res) => {
@@ -49,16 +54,3 @@ export class TransferenciaComponent implements OnInit {
 		this.router.navigate(['/'])
 	}
 }
-
-export interface Statement {
-	type: string;
-	date: string;
-	value: number;
-}
-
-const ESTATEMENT_DATA: Statement[] = [
-	{ type: 'arrow_forward', date: '11/01/2018', value: 1.079, },
-	{ type: 'arrow_back', date: '07/12/2017', value: 4.026, },
-	{ type: 'arrow_forward', date: '10/12/2017', value: 6.041 },
-];
-	
