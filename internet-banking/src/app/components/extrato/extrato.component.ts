346a2807e69f4187b5c87d8cf48a713f43a020ed
Dante Webmaster <dantewebmaster@outlook.com>
2018-01-26 04:23:20 -0200

    Rotas animadas

--- internet-banking/src/app/components/extrato/extrato.component.ts
+++ internet-banking/src/app/components/extrato/extrato.component.ts
@@ -3,11 +3,14 @@ import { ExtratoService } from '../../services/extrato.service';
 import { Globals } from '../../model/Globals.module';
 import { HttpClient } from '@angular/common/http';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { moveIn, fallIn } from '../../router.animations';
 
 @Component({
 	selector: 'app-extrato',
 	templateUrl: './extrato.component.html',
-	styleUrls: ['./extrato.component.scss']
+	styleUrls: ['./extrato.component.scss'],
+	animations: [moveIn(), fallIn()],
+	host: { '[@moveIn]': '' }
 })
 export class ExtratoComponent implements OnInit {
 	
--- internet-banking/src/app/router.animations.ts
+++ internet-banking/src/app/router.animations.ts
@@ -1,18 +1,16 @@
-// Implementação futura, animar components ao carregar a página
-// import { state } from '@angular/core';
+import { trigger, state, animate, style, transition } from '@angular/core';
 
-/*
 export function moveIn() {
 	return trigger('moveIn', [
 		state('void', style({ position: 'fixed', width: '100%' })),
 		state('*', style({ position: 'fixed', width: '100%' })),
 		transition(':enter', [
 			style({ opacity: '0', transform: 'translateX(100px)' }),
-			animate('.6s ease-in-out', style({ opacity: '1', transform: 'translateX(0)' }))
+			animate('1s ease-in-out', style({ opacity: '1', transform: 'translateX(0)' }))
 		]),
 		transition(':leave', [
 			style({ opacity: '1', transform: 'translateX(0)' }),
-			animate('.3s ease-in-out', style({ opacity: '0', transform: 'translateX(-200px)' }))
+			animate('.5s ease-in-out', style({ opacity: '0', transform: 'translateX(-200px)' }))
 		])
 	]);
 }
@@ -21,11 +19,11 @@ export function fallIn() {
 	return trigger('fallIn', [
 		transition(':enter', [
 			style({ opacity: '0', transform: 'translateY(-40px)' }),
-			animate('.5s .3s ease-in-out', style({ opacity: '1', transform: 'translateY(0)' }))
+			animate('1.2s .3s ease-in-out', style({ opacity: '1', transform: 'translateY(0)' }))
 		]),
 		transition(':leave', [
 			style({ opacity: '1', transform: 'translateY(0)' }),
-			animate('.5s ease-in-out', style({ opacity: '0', transform: 'translateY(40px)' }))
+			animate('1s ease-in-out', style({ opacity: '0', transform: 'translateY(40px)' }))
 		])
 	]);
 }
@@ -34,8 +32,7 @@ export function moveInLeft() {
 	return trigger('moveInLeft', [
 		transition(':enter', [
 			style({ opacity: '0', transform: 'translateX(-100px)' }),
-			animate('.6s .2s ease-in-out', style({ opacity: '1', transform: 'translateX(0)' }))
+			animate('1.6s .2s ease-in-out', style({ opacity: '1', transform: 'translateX(0)' }))
 		])
 	]);
 }
-*/
